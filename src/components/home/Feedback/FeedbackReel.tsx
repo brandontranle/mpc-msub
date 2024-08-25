3af9e04171a3f392448c3e06705c65126da932a1
Brandon Tran Le <111268644+brandontranle@users.noreply.github.com>
2024-08-24 21:07:21 -0700

    remove additional feedbacks for marquee

--- src/components/home/Feedback/FeedbackReel.tsx
+++ src/components/home/Feedback/FeedbackReel.tsx
@@ -6,7 +6,7 @@ export const FeedbackReel = () => {
     <section className="flex flex-col items-center justify-center pb-20 relative side-mask">
       <span className="font-bold text-sm mb-5">WHERE OUR ALUMNI ARE</span>
       <div className="w-max flex flex-row gap-5 flex-nowrap overflow-hidden animate-scroll">
-        {[...feedbacks, ...feedbacks, ...feedbacks].map((feedback, index) => (
+        {[...feedbacks, ...feedbacks].map((feedback, index) => (
           <FeedbackCard key={index} img={feedback.path} />
         ))}
       </div>
